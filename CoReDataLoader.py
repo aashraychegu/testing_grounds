96ba30fdfac7ce520dbe37814b133406c828eab5
aashraychegu <50562277+aashraychegu@users.noreply.github.com>
2022-10-30 13:22:32 -0700

    updating with the GAN prototype

--- CoReDataLoader.py
+++ CoReDataLoader.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import os
 import pathlib as p
 import sys
 from typing import List, Union
+from os import devnull as print_supression_source
 
+import numpy as np
 import torch
 from torch.utils.data import *
+from torch.nn.functional import pad as pad_tensor
 
 
 class HiddenPrints:
     def __enter__(self):
         self._original_stdout = sys.stdout
-        sys.stdout = open(os.devnull, "w")
+        sys.stdout = open(print_supression_source, "w")
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         sys.stdout.close()
@@ -23,10 +25,12 @@ class HiddenPrints:
 class CoReDataSet(Dataset):
     def __init__(
         self,
-        local_path: str = p.Path(__file__).parent.absolute() / "CoRe_DB_clone",
+        local_path: str = p.Path(__file__).parent.absolute(
+        ) / "CoRe_DB_clone",  # type: ignore
         rh_xx: str = "rh_22",
+        maxlen=40817,
         attrs: Union[List[str], str] = "*",
-        gw_preprocess_func=lambda x: torch.from_numpy(x),
+        gw_preprocess_func=True,
     ):
         self.local_path = p.Path(local_path)
         if not self.local_path.exists():
@@ -38,7 +42,14 @@ class CoReDataSet(Dataset):
         self.indexes = {}
         self.rh_xx = rh_xx
         self.attrs = attrs
-        self.gw_preprocess_func = gw_preprocess_func
+        self.maxlen = maxlen
+        if gw_preprocess_func == True:
+            self.gw_preprocessor = lambda x: pad_tensor(
+                torch.from_numpy(x), (0, 0, 0, self.maxlen -
+                                      x.shape[0]), "constant", 0
+            )
+        else:
+            self.gw_preprocessor = gw_preprocess_func
         index_tracker = 0
         for sim_key in self.sim:
             sel_sims = self.sim[sim_key]
@@ -52,19 +63,22 @@ class CoReDataSet(Dataset):
 
     def __getitem__(self, idx):
         if not idx in self.indexes.values():
-            raise IndexError(f"the entered index is not valid {idx} {idx.split(':')}")
+            raise IndexError(
+                f"the entered index is not valid {idx} {idx.split(':')}")
         split_idx = idx.split(":")
         sel_sim = split_idx[0] + ":" + split_idx[1]
         sel_run = split_idx[2]
         data = self.sim[sel_sim].run[sel_run].data
         if self.attrs == "*":
             self.attrs = list(data.mdata.data.keys())
-        return self.gw_preprocess_func(data.read(self.rh_xx)), {
-            i: data.mdata.data[i] for i in data.mdata.data if i in self.attrs
-        }
+        return [
+            self.gw_preprocessor(data.read(self.rh_xx)[:, 0:2]),
+            {i: data.mdata.data[i]
+                for i in data.mdata.data if i in self.attrs},
+        ]
 
     def collate_fn(self, data):
-        return tuple(data)
+        return data
 
 
 class CoReSampler(Sampler):
@@ -81,10 +95,11 @@ class CoReSampler(Sampler):
             yield self.indexes[i]
 
 
-c = CoReDataSet(attrs=["id_eos", "id_mass_starA", "id_mass_starB", "database_key"])
+c = CoReDataSet(attrs=["id_eos", "id_mass_starA",
+                "id_mass_starB", "database_key"])
 dataloader = DataLoader(
-    c, batch_size=64, sampler=CoReSampler(c.indexes, len(c)), collate_fn=c.collate_fn
+    c, batch_size=1, sampler=CoReSampler(c.indexes, len(c)), collate_fn=c.collate_fn
 )
 
 if __name__ == "__main__":
-    print(next(iter(dataloader))[0][1]["database_key"])
+    print(next(iter(dataloader))[0][0].shape)
