b4809ef5a00e87b2a1bfb5bb9ce823253061a09a
aashraychegu <50562277+aashraychegu@users.noreply.github.com>
2023-01-18 15:15:10 -0800

    finished the m1m2classifier

--- CoReDataLoader.py
+++ CoReDataLoader.py
@@ -122,9 +122,8 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 dataset = CoRe_DB_Dataset(sync=False, device=device)
 dataloader = DataLoader(
     dataset,
-    batch_size=16,
+    batch_size=32,
     shuffle=True,
-    num_workers=4
 )
 if __name__ == "__main__":
     print(next(iter(dataloader)))
